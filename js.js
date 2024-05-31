14df4ee11106623da5e66cd1fbb528076d5937c3
SirEvix <146931634+SirEvix@users.noreply.github.com>
2024-05-31 21:22:15 +0200

    k

---
+++ js.js
@@ -0,0 +1,32 @@
+// Step 1: Create a New Repository
+
+    // Go to GitHub and sign in to your account.
+    // Click the ‘+’ button in the upper-right corner and select ‘New repository’.
+    // Enter a name for your repository, choose whether it should be public or private, and click ‘Create repository’.
+// Step 2: Clone the Repository Locally
+
+    // On the main page of your new repository, click the ‘Code’ button and copy the URL under ‘Clone with HTTPS’.
+    // Open a terminal on your computer, navigate to the directory where you want to clone the repository using the cd command, and then run git clone [URL], replacing [URL] with the URL you copied.
+// Step 3: Open the Repository in Visual Studio Code
+
+    // Open Visual Studio Code.
+    // Click ‘File’ in the top menu and select ‘Open Folder’.
+    // Navigate to the directory where you cloned your repository and click ‘Select Folder’.
+// Step 4: Open a Terminal in Visual Studio Code
+
+    // Click ‘Terminal’ in the top menu and select ‘New Terminal’. This will open a new terminal at the bottom of Visual Studio Code.
+// Step 5: Initialize a New Vite Project
+
+    // In the terminal, type npm init vite and press Enter.
+    // The terminal will prompt you to enter a project name. Type the name of your project and press Enter.
+    // Next, the terminal will prompt you to select a template. You can use the arrow keys to scroll through the options and press Enter to select one.
+    // Once you’ve selected a template, the terminal will create your new project.
+// Step 6: Install Project Dependencies
+
+    // In the terminal, navigate into your new project directory using the cd command.
+    // Once you’re in your project directory, type npm install three and press Enter. This command will install all the dependencies that your project needs.
+// Step 7: Start the Development Server
+
+// In the terminal, make sure you’re still in your project directory.
+// Type npm run dev and press Enter. This command will start the development server.
+// After following these steps, your new project should be set up and running. You can open your web browser and go to http://localhost:5000 to see your new project.
